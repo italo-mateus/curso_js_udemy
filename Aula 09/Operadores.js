ef479630e283ec67e3a8d4cab9f1e1b1ab6267ac
italo-mateus <italomtitus@gmail.com>
2023-04-18 23:26:02 -0300

    Create Operadores.js

---
+++ Aula 09/Operadores.js
@@ -0,0 +1,20 @@
+/*
+operadores aritmeticos, de atribuição e incremento
+
++ adução / concatenação
+
+Existem também: 
+- , * , ** (Potenciação) , % (Resto da divisão)
+
+Operador de incremento (Também existe o de Decremento, os dois podem conter o ++ antes e depois)
+
+Operadores de atribuição
+*/
+
+// NaN - Not a number
+// parseInt (Inteiro), parseFloat (Decimais)
+
+const num1 = 10
+const num2 = Number('5.2')
+console.log(num1 + num2)
+console.log(typeof num2)
