d4251a1d06c0da852fc39e35267866bd18c0a0e9
italo-mateus <italomtitus@gmail.com>
2023-04-24 21:46:46 -0300

    JS

---
+++ Aula 10 - Desafio/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Digite um número')
+num1 = parseFloat(num1)
+let num2 = prompt('Digite um número')
+num2 = parseFloat(num2)
+let num3 = num1 + num2
+alert(`O resultado da soma é ${num3}`)
---
+++ Aula 11/index.js
@@ -0,0 +1,14 @@
+let num1 = 1
+let num2 = 2.5
+
+//console.log(num1.toString() + num2) Converte o Número em String
+
+//num1 = num1.toString() Converte o Número em String
+
+//console.log(num1.toFixed(2)) /*Diminuir casas decimais */
+
+//console.log(Number.isInteger(num1)) /*Verifica se um número é inteiro (true) ou Quebrado (False) */
+
+/*let temp = num1 * 'olá'
+console.log(Number.isNaN(temp)) /*Consulta se a operação é NaN */
+
