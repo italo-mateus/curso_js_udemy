19251b485ad711b06f7c15349359288b7a7de8c0
italo-mateus <italomtitus@gmail.com>
2023-04-25 22:40:24 -0300

    Math

---
+++ Aula 12/index.js
@@ -0,0 +1,10 @@
+let num1 = 9.54
+//let num2 = Math.floor(num1) /*Aredonda o número pra baixo*/
+//let num2 = Math.ceil(num1) /*Aredonda o número pra cima*/
+//let num2 = Math.round(num1) /*Aredonda pro mais proximo */
+
+//console.log(Math.max(1,2,3,4,5,6,7,8,9)) /*Exibe o número maximo */
+//console.log(Math.min(1,2,3,4,5,6,7,8,9)) /*Exibe o número minimo */
+
+const aleat = Math.round(Math.random() * (10 - 5)) + 5;
+console.log(aleat)
