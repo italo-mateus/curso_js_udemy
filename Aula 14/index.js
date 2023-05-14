6dc15366af96afd96697e25d0d7f005ce2ebaa9e
italo-mateus <italomtitus@gmail.com>
2023-05-14 13:12:39 -0300

    Function

---
+++ Aula 14/index.js
@@ -0,0 +1,16 @@
+
+//Funcionamento de uma função
+/*function saudacao(nome) {
+console.log(`Bom dia ${nome}`)
+}
+
+saudacao('Luiz')*/
+
+function soma(a = 1,b = 1) { //Define valores a função
+    const resultado = a + b
+    return resultado //Nada é executado depois de return dentro da function
+} 
+
+console.log(soma(2 , 2))
+
+// Arrow function
