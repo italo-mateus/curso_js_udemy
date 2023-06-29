2dadd4a87b450c00bbf18a9fcc76bd5b4dee5adc
Italo Mateus <italomtitus@gmail.com>
2023-06-28 22:31:26 -0300

    Organização das pastas + conteúdo

---
+++ JS Lógica de programação/Operadores de conparação.js
@@ -0,0 +1,13 @@
+/*Operadores de conparação
+> Maior que
+>= Maior ou igual a
+< Menor que
+<= Menor ou igual a
+== Igualdade (Não recomendado usar)
+=== Igualdade estrita ( checa Valor e tipo)
+!= diferença (Não recomendado usar)
+!== diferente estrito (checa valor e tipo)
+*/
+
+const comp = 10>= 5;
+console.log(comp);
