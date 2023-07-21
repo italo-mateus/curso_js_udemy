e5cc41d79f7f94ceb015c7561baa3d7ad0081aac
Italo Mateus <italomtitus@gmail.com>
2023-07-20 23:39:49 -0300

    Exercício JS

---
+++ JS para iniciantes/Exercicio_array_função_objeto/main.js
@@ -0,0 +1,30 @@
+function meuescopo (){
+    const formulario = document.querySelector('.formulario');
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = [];
+
+    function recebeevento (evento){
+
+        evento.preventDefault();
+
+        const nome = formulario.querySelector('.nome');
+        const peso = formulario.querySelector('.peso');
+        const altura = formulario.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${peso.value} ${altura.value}</P>`
+    }
+
+    formulario.addEventListener('submit', recebeevento);
+
+}
+
+meuescopo();
