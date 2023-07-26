a7850e6a6764ffe0e168fd96bb7ff86c34c684bb
Italo Mateus <italomtitus@gmail.com>
2023-07-25 23:02:07 -0300

    Object Data

---
+++ JS Lógica de programação/Operação ternária.js
@@ -0,0 +1,13 @@
+//Operação  ternária só vale em duas opões, sim ou não
+// (Condição) ? 'Valor para verdadeiro' : 'Valor para falso';
+
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal'
+
+/*
+if (pontuacaoUsuario >=1000){
+    console.log('Usuário VIP')
+} else {
+    console.log ('usuário normal')
+}
+*/
