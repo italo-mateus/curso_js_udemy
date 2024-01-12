e25e3a493f88da898bfd77b5c38a990e01acb8bd
Italo Mateus <italomtitus@gmail.com>
2024-01-11 22:50:34 -0300

    Update getters_setters.js

--- Objeto/getters_setters.js
+++ Objeto/getters_setters.js
@@ -1,32 +1,22 @@
+//Metodo GET Obtem o valor e SET configura o valor recebido na chave do objeto
+// Utilizando GET e SET não faz sentido utilizar o value e writable
+
 function Produto(nome, preco, estoque) {
     this.nome = nome
     this.preco = preco
     this.estoque = estoque
 
     Object.defineProperty(this, 'estoque', {
-        enumerable: true, 
-        writable: false,
-        configurable: true, 
+        get: function(){
+            return estoque
+        },
+        set: function(valor){
+            console.log(valor)
+        }
     })
 }
 
-    Object.defineProperties(this, {
-        nome: {
-            enumerable: true,
-            writable: true,
-            configurable: true
-        },
-        preço: {
-            enumerable: true,
-            writable: true,
-            configurable: true
-        },
-        estoque: {
-            enumerable: true,
-            writable: true,
-            configurable: true
-        }
-})
-
 const produto1 = new Produto('Arroz', 12, 1)
 console.log(produto1)
+produto1.estoque = 'O valor é 1000'
+console.log(produto1.estoque)
